e8fb5c2500020cccf4856e9bcbfd74d4e2e97816
ghost-stack99 <arpandas.kr@gmail.com>
2025-05-24 00:16:05 +0530

    update

--- nova-frontend/app/login/page.tsx
+++ nova-frontend/app/login/page.tsx
@@ -1,13 +1,21 @@
-import { LoginForm } from "@/components/login-form"
-import { app, analytics } from "@/lib/firebase";
+"use client"; // if this is a client component
 
+import { logEvent } from "firebase/analytics";
+import { analytics } from "@/lib/firebase";
+import LoginForm from "./login-form";
 
 export default function LoginPage() {
+  React.useEffect(() => {
+    if (analytics) {
+      logEvent(analytics, "login_page_view");
+    }
+  }, []);
+
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
         <LoginForm />
       </div>
     </div>
-  )
+  );
 }
